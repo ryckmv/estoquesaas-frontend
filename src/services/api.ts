245500c5ec6eaf656998a63b3229975ab74e6531
Henrique Cruz <rick_mv@hotmail.com>
2026-07-28 23:10:03 -0300

    Preparar frontend para produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3334",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "https://estoquesaas-backend.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
